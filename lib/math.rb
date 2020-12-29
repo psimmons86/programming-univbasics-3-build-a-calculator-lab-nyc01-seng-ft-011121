9bbff05aabfb25b9abc061985f14b793ac5dc322
patrick simmons <hadroncollides@gmail.com>
2020-12-29 00:31:07 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -8,15 +8,15 @@ num2 - num1
 end
 
 def division(num1, num2)
-num1 * num2
+num1 / num2
 end
 
 def multiplication(num1, num2)
-
+num1 * num2
 end
 
 def modulo(num1, num2)
-
+num2 % num1
 end
 
 def square_root(num)
