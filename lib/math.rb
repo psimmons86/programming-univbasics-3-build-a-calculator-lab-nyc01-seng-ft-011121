896786f567f034ef56667769939b2a3623de47a7
patrick simmons <hadroncollides@gmail.com>
2020-12-29 00:34:44 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,21 +4,21 @@ return num2 + num1
 end
 
 def subtraction(num1, num2)
-num2 - num1
+return num2 - num1
 end
 
 def division(num1, num2)
-num1 / num2
+return num1 / num2
 end
 
 def multiplication(num1, num2)
-num1 * num2
+return num1 * num2
 end
 
 def modulo(num1, num2)
-num2 % num1
+return num2 % num1
 end
 
 def square_root(num)
-
+num Math.sqrt
 end
